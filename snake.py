2c5faf06ad0708b0423b2f17e1407f0eea52c0bc
CarlaOnate <carlaonategardella@gmail.com>
2022-05-10 10:19:33 -0500

    Moving food feature done

--- snake.py
+++ snake.py
@@ -1,10 +1,35 @@
 from turtle import *
 from random import randrange
+from wsgiref import headers
 from freegames import square, vector
 
-food = vector(0, 0)
+food = vector(100, 100)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+
+def moveFood():
+    head = snake[-1].copy() # Find snake position
+    axis = randrange(0, 2) # To change position in the x axis or y axis
+    direction = randrange(0, 3) # To add or subtract from position
+    if not food == head and inside(food):
+        if axis == 0: # if axis 0 then change x axis
+            if direction == 0:
+                food.x += 10 # if direction 0 then sum
+            else:
+                food.x -= 10 # if direction 0 then subtract
+        elif axis == 1: # if axis 1 then change y axis
+            if direction == 0:
+                food.y += 10
+            else:
+                food.y -= 10
+    elif not inside(food):
+        food.x = 0 # if not inside then go to start position
+        food.y = 0
+
+def triggerFood(): # recursive loop to change food every 500 milliseconds
+    moveFood() # call move food
+    update() # update canvas
+    ontimer(triggerFood, 500) # create timer to call again triggerFood to move the food again
 
 def change(x, y):
     "Change snake direction."
@@ -52,4 +77,5 @@ onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
 move()
+triggerFood() # function to trigger moveFood function
 done()
